d094d3ec2b2d4ae7347da2f6c0883bf1b733d982
Jun <jlee757@uic.edu>
2017-11-24 00:51:38 -0600

    ion-icon

--- Weather.js
+++ Weather.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from "expo";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class Weather extends Component{
     render(){
@@ -11,7 +12,7 @@ export default class Weather extends Component{
         >
 
         <View style={styles.upper}>
-            <Text>Icon here!</Text>
+            <Ionicons color="white" size={144} name="ios-rainy"/>
             <Text style={styles.temp}>35˚C</Text>
         </View>
 
@@ -33,10 +34,11 @@ const styles = StyleSheet.create({
     upper: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: 'transparent'
     },
     temp: {
-        fontSize: 38,
+        fontSize: 48,
         backgroundColor: 'transparent',
         color: 'white',
         marginTop: 10
